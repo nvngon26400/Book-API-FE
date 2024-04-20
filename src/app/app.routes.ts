95c17acb1fa854d5d5f51c03381c15d7a1fd05b7
26400 <nvngon2604@gmail.com>
2024-04-20 09:54:10 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { BookComponentComponent } from './book-component/book-component.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'book', component: BookComponentComponent } //path to the book component
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class BookComponent { }
+
---
+++ src/app/book-component/book-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-component.component.html',
+  styleUrl: './book-component.component.css'
+})
+export class BookComponentComponent {
+
+}
